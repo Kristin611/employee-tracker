ca6e304631aca4792a1235dcddd8c50270bf80b5
Kris10 <kmrichie611@gmail.com>
2023-11-13 11:14:48 -0600

    added env file

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const logo = require('asciiart-logo');
+const dotenv = require('dotenv');
+dotenv.config()
 
 require('console.table')
 
+//to connect to mysql databases 
 const db = mysql.createConnection(
     {
-      host: "localhost",
-      user: "root",
-      password: "Pantheon4ever!",
-      database: "employees_db",
+      host: "127.0.0.1",
+      user: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PW,
+      database: process.env.MYSQL_DB,
     },
     console.log(`Connected to employees_db database.`)
   );
