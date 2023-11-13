e40106563200e24cc05c23121f766361ab1cab5b
Kris10 <kmrichie611@gmail.com>
2023-11-13 11:00:56 -0600

    added mysql connection to server

--- server.js
+++ server.js
@@ -1,4 +1,24 @@
 const inquirer = require('inquirer');
-const express = require('express');
 const mysql = require('mysql2');
+const logo = require('asciiart-logo');
 
+require('console.table')
+
+const db = mysql.createConnection(
+    {
+      host: "localhost",
+      user: "root",
+      password: "Pantheon4ever!",
+      database: "employees_db",
+    },
+    console.log(`Connected to employees_db database.`)
+  );
+
+  function init() {
+    const logoText = logo({name: 'Employee Tracker'}).render()
+    console.log(logoText);
+
+    
+  }
+
+  init(); 
